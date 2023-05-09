f44711c69a84a93e0444793aaa537042cec67b5f
Arturo9314 <arturo140493@gmail.com>
2023-05-09 05:44:42 -0600

    fix fetch api

--- src/services/conectAPI.js
+++ src/services/conectAPI.js
@@ -1,5 +1,6 @@
-const ADVICE_RANDOM = 'https://api.adviceslip.com/advice'
 const getRandomAdvice = async ()=>{
+    const randomNumber = Math.floor(Math.random() * 2);
+    const ADVICE_RANDOM = `https://api.adviceslip.com/advice?random${randomNumber}`    
     const res = await fetch(ADVICE_RANDOM)
     const data = await res.json()
     return {id: data.slip.id, advice: data.slip.advice}
